8f0b73df4f9c4293c385d02c719afc1d7fc7f550
Arafat Patwary <farabipsk@gmail.com>
2026-03-25 12:05:08 +0600

    Practice file

---
+++ warmUp/dist/test.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let course = "Next Level Web Development";
+console.log(course);
+// Data type
+let firstCode = 'C++ Code';
+//number
+let roll = 321;
+//boolean
+let isAdmin = true;
+//null
+let valueAssign = null;
+//undefined
+let undefinedV = undefined;
+length;
+//Check 
+let d;
+// d = 123;
+// d= "str";
+d = true;
+//? Areray
+let friends = ['Ro', 'go'];
+let nombor = [2, 3, 4];
+nombor[0] = 88;
+//tuple
+//array->order->-->type of value
+let tupleValue = [32, '54'];
+//* Reference type object
+const user = {
+    company: "Coding Master",
+    firstName: "Demne",
+    lastName: "Gregor"
+};
+//? Learning Function
+//Normal function
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(23, 44);
+//Arrow function
+const addArrow = (val1, val2) => val1 + val2;
+addArrow(33, 44);
+//# sourceMappingURL=test.js.map
---
+++ warmUp/src/test.ts
@@ -0,0 +1,70 @@
+let course:String = "Next Level Web Development"
+console.log(course)
+
+// Data type
+let firstCode: string = 'C++ Code'
+//number
+let roll: number = 321
+//boolean
+let isAdmin: boolean = true;
+//null
+let valueAssign: null = null;
+//undefined
+
+let undefinedV:undefined = undefined;length
+//Check 
+let d:boolean;
+// d = 123;
+// d= "str";
+d= true;
+
+//? Areray
+let friends: string[]= ['Ro','go']
+let nombor: number[]= [2,3,4];
+nombor[0]= 88;
+
+//tuple
+//array->order->-->type of value
+let tupleValue:[number,string]=[32,'54']
+
+//* Reference type object
+
+const user:{
+    company: "Coding Master";   //type--? behave as "literals type"
+    firstName: string;
+    middleName?: string;  //optional
+    lastName: string;
+
+}={
+    company: "Coding Master",
+    firstName:"Demne",
+    
+    lastName: "Gregor"
+}
+
+//? Learning Function
+//Normal function
+
+function add(num1:number,num2:number):number{
+    return num1 + num2;
+}
+add(23,44);
+
+//Arrow function
+
+const addArrow=(val1:number,val2:number):number => val1 + val2
+addArrow(33,44);
+
+//object --> function --> method
+
+const myBalence ={
+    name: "Patwary",
+    balance: 0,
+
+    addBalance(balance:number):string{
+        return `My Balance ${this.balance +balance}`;
+    }
+}
+
+const arr:number[]=[3,4,5];
+const newArr:number[]= arr.map((elem:number):number => elem *elem)
